5ecf765fcf933595399999b421438f8a9215fd02
abhinav2525 <ec22064@qmul.ac.uk>
2023-11-07 19:45:08 +0000

    routes

---
+++ app/ClientMember/page.jsx
@@ -0,0 +1,12 @@
+
+"use client";
+const Member = () => {
+  return (
+    <div>
+      <h1>Member Client Member</h1>
+    </div>
+  );
+}
+
+export default Member;
+
---
+++ app/CreateUser/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateUser = () => {
+  return <div className="text-red-400">Only Admins!</div>;
+}
+
+export default CreateUser;
